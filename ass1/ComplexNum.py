3f7c1338f8fff8c0406186eb11ec1ef9f5dd6912
haiatn <nimnimch@gmail.com>
2019-12-06 23:19:23 +0200

    q2,3 odds

--- ass1/ComplexNum.py
+++ ass1/ComplexNum.py
@@ -1,6 +1,10 @@
 import math
 
+from functools import reduce
 
+'''
+ this class represent a complex number which is a real number which can have an imaginary part
+'''
 class ComplexNum:
     # 1.1 - constructor for the complex number
     # re - real part of the complex number
@@ -32,10 +36,10 @@ class ComplexNum:
             im = str(self.im)
         return str(self.re) + " " + sign + " " + im + "i"
 
-    # 1.5
+    # 1.5 - return whether two objects are logically same
     def __eq__(self, other):
         if type(other) == type(self):
-            if other[0] == self.re and other[1] == self.im:
+            if other.re == self.re and other.im == self.im:
                 return True
             else:
                 return False
@@ -51,10 +55,10 @@ class ComplexNum:
     def __neg__(self):
         return ComplexNum(-1 * self.re, -1 * self.im)
 
-    # 1.7 -
+    # 1.7 - calculate and return the result of subtracting the complex number from the other given value
     def __sub__(self, other):
         # I need to check other's type and follow accordingly (?)
-        return other.__add__(self.__neg())
+        return self.__add__(other.__neg__())
 
     # 1.8 - calculate and return the multiplication of two complex numbers
     def __mul__(self, other):
@@ -73,13 +77,136 @@ class ComplexNum:
         return math.sqrt(mul.re + mul.im)
 
 
-# tests
-z = ComplexNum(1, 2)
-print(z.re)
-print(z.im)
-print(z)
-print(z + ComplexNum(1, -3))
-print(z.conjugate())
-print(-z)
-print(z.abs())
-print(z+2)
+# 2.1 - return whether object1 is an instance of classInfo
+def isInstancePPL(object1,classInfo):
+    return (type(classInfo) is type) and (type(object1) is not type) and (object1.__class__ is classInfo or classInfo in object1.__class__.__bases__)
+
+# 2.2 - return the number of hirarchy level between object1 class and classInfo
+def numInstancePPL(object1,classInfo):
+    #TODO
+    None
+
+# 2.3 - constructor for the complex number
+def isSubclassPPL(class1,classInfo):
+    return (type(classInfo) is type) and (type(class1) is type) and (classInfo in class1.__bases__ or class1==classInfo)
+
+# 2.4 - return the number of hirarchy level between class1 and classInfo
+def numSubclassPPL(class1,classInfo):
+    #TODO
+    None
+
+
+# 3.1 - return number of object in list which statisfy func1 condition
+def count_if(lst,func):
+    return list(map(func,lst)).count(True)
+
+# 3.2 - return if the condition func2 is true for calculating func1 on each list item
+def for_all(lst,func1,func2):
+    # TODO
+    None
+
+# 3.3 - return if the condition func2 is true for calculating func1 on all list items
+def for_all_red(lst,func1,func2):
+    return func2(reduce(func1,lst))
+
+# 3.4 - return if exists n items in the list who statisfy func1
+def there_exists(lst,n,func1):
+    # TODO
+    None
+
+
+
+def testQ1():
+    # tests
+    z = ComplexNum(1, 2)
+    print(z.re) #1
+    print(z.im) #2
+    print(z) #1 + 2i
+    print(z + ComplexNum(1, -3)) #2 - 1i
+    print(z.conjugate()) # 1 - 2i
+    print(-z) #-1 - 2i
+    print(z.abs()) #sqrt(5)
+    print(z+2) # some kind of fail
+
+    z2 = ComplexNum(3, 1)
+    print(z2.re) #3
+    print(z2.im) #1
+    print(z2) #3 + 1i
+    print(z+z2) #4 + 3i
+    print(z-z2) #-2 +1i
+    #print(2-z2) #some kind of fail (we did not neede to actually implement)
+    print(z2-3) #some kind of fail
+    print(z2-3.0) #some kind of fail
+    print(z.conjugate()+z2) #4 - 1i
+    print(z2*z) #1 + 7i
+    print((z2*z).abs()) # sqrt(50)
+    print(z2.tu_tuple()) # (3,1)
+    print(z.__eq__(z2)) #false
+    print(z==z2) #false
+    print(z2==z) #false
+    print((z-z)==(z2-z2)) #true
+    print(z2.__eq__(2)) #false
+    print(z2.__eq__(2.0)) #false
+    print(ComplexNum(0,0)) # 0 + 0i
+    print(type(z)==type(z2)) #true (we did not neede to actually implement, I just made sure
+    #print(z2*3) #raise error
+    #print(2*z2) #some kind of error (we did not neede to actually implement)
+
+
+
+def testQ2():
+    class X:
+        pass
+    class Y(X):
+        pass
+    x=X()
+    y=Y()
+
+    print(isInstancePPL(x,X)) #True
+    print(isInstancePPL(x, Y)) #False
+    print(isInstancePPL(y, X)) #True
+    print(isInstancePPL(y, Y)) #True
+
+    print(numSubclassPPL(y, type(x)))  # 2
+    print(numSubclassPPL(y, Y))  # 1
+    print(numSubclassPPL(x, type(y)))  # 0 if im correct
+
+    print(isSubclassPPL(x.__class__, X)) #True
+    print(isSubclassPPL(X, Y)) #False
+    print(isSubclassPPL(Y, X)) #True
+    print(isSubclassPPL(Y, type(y))) #True
+
+
+    print(numSubclassPPL(Y,type(x))) #2
+    print(numSubclassPPL(y.__class__, Y)) #1
+
+    class Z(Y):
+        pass
+    z = Z()
+    print(isInstancePPL(x, Z)) #False
+    print(isInstancePPL(y, Z)) #False
+    print(isInstancePPL(z, X)) #True TODO
+    print(isInstancePPL(z, Y)) #True
+
+    print(numInstancePPL(z,X))
+    print(numInstancePPL(x,Z))
+
+    print(isSubclassPPL(X, Z)) #False
+    print(isSubclassPPL(Y, Z)) #False
+    print(isSubclassPPL(Z, X)) #True TODO
+
+    print(numSubclassPPL(X,Z))
+    print(numSubclassPPL(Z, X))
+
+def testQ3():
+    print(count_if([1,0,8], lambda x: x > 2)) #1
+    print(count_if([1, 1, 8], lambda x: x == 1))  # 2
+
+    print(for_all([1, 0, 8], lambda x: x * 2, lambda x: x > 0))  # False
+    print(for_all([1, 1, 8], lambda x: x, lambda x: x > 0))  # True
+
+    print(for_all_red([1, 0, 8], lambda x,y: x * y, lambda x: x > 0))  # False
+    print(for_all_red([1, 1, 8], lambda x,y: x * y, lambda x: x > 7))  # True
+
+    print(there_exists([1,0,8],2,lambda x:x>-1)) #True if im correct
+
