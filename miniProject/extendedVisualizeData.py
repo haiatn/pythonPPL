6b15a7c508d4c30cec0eaa4aa653feffe7381cf6
nimrodhaiat <nimnimch@gmail.com>
2019-12-15 14:24:59 +0200

    mini-project tooltip for normalized

--- miniProject/extendedVisualizeData.py
+++ miniProject/extendedVisualizeData.py
@@ -5,83 +5,97 @@ from bokeh.plotting import figure
 from bokeh.layouts import column
 from bokeh.io import curdoc
 import colorcet as cc
+'''
 
+
+'''
 
 def getGenres(type):
     with sqlite3.connect("spotifyGraphDB.sqlite") as con:
         con.row_factory = lambda cursor, row: row[0]
         cur = con.cursor()
-        table='songs'
-        if type=='Normalized':
-            table='NormalizedSongs'
-        cur.execute("select distinct genre from "+table)
+        table = 'songs'
+        if type == 'Normalized':
+            table = 'NormalizedSongs'
+        cur.execute("select distinct genre from " + table)
         return cur.fetchall()
+
 
 def getDates():
     return pd.date_range(start="2018-10-1", end="2019-10-31", freq='D').tolist()
 
-def getPopularityByGenre(genre,type):
+
+def getPopularityByGenre(genre, type):
     with sqlite3.connect("spotifyGraphDB.sqlite") as con:
         table = 'songs'
         if type == 'Normalized':
             table = 'NormalizedSongs'
-        con.row_factory=lambda cursor, row: row[0]
+        con.row_factory = lambda cursor, row: row[0]
         cur = con.cursor()
-        cur.execute("select COALESCE(avg, 0) as avg from (select distinct songRate.rateDate from songRate) as dates left join (select (30- Avg(position))/31 as avg, rateDate from (select id from "+table+" where genre like '"+genre+"') as ids join songRate on ids.id = songRate.id group by rateDate order by rateDate) as actualResult on dates.rateDate = actualResult.rateDate")
+        cur.execute(
+            "select COALESCE(avg, 0) as avg from (select distinct songRate.rateDate from songRate) as dates left join (select (30- Avg(position))/31 as avg, rateDate from (select id from " + table + " where genre like '" + genre + "') as ids join songRate on ids.id = songRate.id group by rateDate order by rateDate) as actualResult on dates.rateDate = actualResult.rateDate")
         return cur.fetchall()
 
-def getTooltipDataForGenre(genre,title,type):
+
+def getTooltipDataForGenre(genre, title, type):
     with sqlite3.connect("spotifyGraphDB.sqlite") as con:
-        con.row_factory=lambda cursor, row: row[0]
+        con.row_factory = lambda cursor, row: row[0]
         cur = con.cursor()
         table = 'DailyGenrePercents'
         if type == 'Normalized':
-            #table = 'NormalizedDailyGenrePercents'
-            return [0]*370
-            1+1
-        cur.execute("select "+title+" from "+table+" where genre = '"+genre+"'")
+            table = 'normalizedDailyGenrePercents'
+        cur.execute("select " + title + " from " + table + " where genre = '" + genre + "'")
+
         def fixZeros(x):
-            if x==None:
+            if x == None:
                 return 0
             else:
                 return x
-        return list(map(lambda x:fixZeros(x),list(cur.fetchall())))
+
+        return list(map(lambda x: fixZeros(x), list(cur.fetchall())))
+
 
 def ridge(category, data, scale=1):
-    data=list(zip([category]*len(data), scale*data))
-    #replace last value with value 0. for graphic reasons
-    data.pop(len(data)-1)
-    data.append((category,0))
+    data = list(zip([category] * len(data), scale * data))
+
+    # replace last value with value 0. for graphic reasons
+    data.pop(len(data) - 1)
     data.append((category, 0))
+    data.append((category, 0))
+
     # replace first value with value 0. for graphic reasons
     data.pop(0)
-    data.insert(0,(category,0))
+    data.insert(0, (category, 0))
     return data
 
-def update_plot(attrName,old,new):
-    if old==new:
+
+def update_plot(attrName, old, new):
+    if old == new:
         return
-    type=new
+    type = new
+
     genres = list(getGenres(type))
-    palette = [cc.rainbow[i*15 % cc.rainbow.__len__()] for i in range(genres.__len__())]
+    palette = [cc.rainbow[i * 15 % cc.rainbow.__len__()] for i in range(genres.__len__())]
     x = getDates()
-    if curdoc().get_model_by_name('mainPlot')!=None:
+    if curdoc().get_model_by_name('mainPlot') != None:
         curdoc().clear()
-        # mainPlotObject=curdoc().get_model_by_name('mainLayout')
-        # curdoc().get_model_by_name('mainLayout').children.remove(mainPlotObject)
-        #rangePlotObject = curdoc().get_model_by_name('rangePlot')
-        #curdoc().get_model_by_name('mainLayout').children.remove(rangePlotObject)
-    p = figure(name="mainPlot",y_range=genres, x_axis_type="datetime",x_range=(x[0], x[len(x)-1]),plot_height=500,plot_width=1000, x_axis_label="date", y_axis_label = "genres")
-    p.add_tools(HoverTool(tooltips=[("low energy", "@energyLowPercent"), ("high energy", "@energyHighPercent"),("slow tempo", "@tempoSlowPercent"), ("medium tempo", "@tempoMediumPercent"), ("fast tempo", "@tempoFastPercent")]))
+
+    p = figure(name="mainPlot", y_range=genres, x_axis_type="datetime", x_range=(x[0], x[len(x) - 1]), plot_height=500,
+               plot_width=1000, x_axis_label="date", y_axis_label="genres")
+    p.add_tools(HoverTool(tooltips=[("low energy", "@energyLowPercent"), ("high energy", "@energyHighPercent"),
+                                    ("slow tempo", "@tempoSlowPercent"), ("medium tempo", "@tempoMediumPercent"),
+                                    ("fast tempo", "@tempoFastPercent")]))
+
     for i, genre in enumerate(reversed(genres)):
-        y = ridge(genre, getPopularityByGenre(genre,type))
-        if type=='Full':
-            source = ColumnDataSource(data={'x': x, 'y': y,'energyLowPercent':getTooltipDataForGenre(genre,'energyLowPercent',type),'energyHighPercent':getTooltipDataForGenre(genre,'energyHighPercent',type),'tempoSlowPercent':getTooltipDataForGenre(genre,'tempoSlowPercent',type),'tempoMediumPercent':getTooltipDataForGenre(genre,'tempoMediumPercent',type),'tempoFastPercent':getTooltipDataForGenre(genre,'tempoFastPercent',type)})
-        else:
-            source = ColumnDataSource(data={'x': x, 'y': y})
+        y = ridge(genre, getPopularityByGenre(genre, type))
+        source = ColumnDataSource(
+                data={'x': x, 'y': y, 'energyLowPercent': getTooltipDataForGenre(genre, 'energyLowPercent', type),
+                      'energyHighPercent': getTooltipDataForGenre(genre, 'energyHighPercent', type),
+                      'tempoSlowPercent': getTooltipDataForGenre(genre, 'tempoSlowPercent', type),
+                      'tempoMediumPercent': getTooltipDataForGenre(genre, 'tempoMediumPercent', type),
+                      'tempoFastPercent': getTooltipDataForGenre(genre, 'tempoFastPercent', type)})
         p.patch(x='x', y='y', color=palette[i], alpha=0.6, line_color="black", source=source)
-        if type=='Full':
-            p.scatter(x='x', y='y', color=palette[i], alpha=0.6, line_color="black", source=source)
+        p.scatter(x='x', y='y', color=palette[i], alpha=0.6, line_color="black", source=source, size=2)
 
     p.outline_line_color = None
     p.background_fill_color = "#efefef"
@@ -99,7 +113,7 @@ def update_plot(attrName,old,new):
     select = figure(title="Drag the middle and edges of the selection box to change the range above",
                     plot_height=130, plot_width=800, y_range=p.y_range,
                     x_axis_type="datetime", y_axis_type=None,
-                    tools="", toolbar_location=None, background_fill_color="#efefef",name="rangePlot")
+                    tools="", toolbar_location=None, background_fill_color="#efefef", name="rangePlot")
 
     range_tool = RangeTool(x_range=p.x_range)
     range_tool.overlay.fill_color = "navy"
@@ -111,10 +125,11 @@ def update_plot(attrName,old,new):
     select.toolbar.active_multi = range_tool
 
     # add option bar for full, normalzed and add to row
-    distribution_select = Select(value=type, title='Genres Viewed', options=['Full', 'Normalized'],name="selectType")
+    distribution_select = Select(value=type, title='Genres Viewed', options=['Full', 'Normalized'], name="selectType")
     distribution_select.on_change('value', update_plot)
-    #show(column(distribution_select,p, select))
-    curdoc().add_root(column(distribution_select,p, select,name="mainLayout"))
+
+    curdoc().add_root(column(distribution_select, p, select, name="mainLayout"))
+
 
 ###########################################Main
-update_plot(None,None,'Full')
+update_plot(None, None, 'Normalized')
--- miniProject/prepareData.py
+++ miniProject/prepareData.py
@@ -11,16 +11,15 @@ os.environ["SPOTIPY_CLIENT_SECRET"] = "aa0fa9dbf7de4ab7b653f15da895c3e3"
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-def getGenres():
+def getGenres(table):
     with sqlite3.connect("spotifyGraphDB.sqlite") as con:
         con.row_factory = lambda cursor, row: row[0]
         cur = con.cursor()
-        cur.execute("select distinct genre from songs")
+        cur.execute("select distinct genre from "+table)
         return cur.fetchall()
 
 def getDates():
     return pd.date_range(start="2018-10-1", end="2019-10-31", freq='D').tolist()
-    #return pd.date_range(start="2019-10-1", end="2019-10-03", freq='D').tolist()
 
 def getEneryDisc(energy):
     if energy > 0.6:
@@ -60,30 +59,30 @@ def rowInsert(cur, row):
     rateDate = str(row['date'])
     cur.execute("insert into songRate (id, streams, rateDate, position, region) values (?, ?, ?, ?,?)", [id, streams, rateDate, position, "il"])
 
-def calculatePercents(cur):
-    cur.executescript("drop table if exists dailyGenrePercents")
-    cur.execute("create table dailyGenrePercents(genre text, date text, tempoSlowPercent float , tempoMediumPercent float , tempoFastPercent float , energyHighPercent float , energyLowPercent float )")
-    genres = getGenres()
+def calculatePercents(cur,table,destTable):
+    cur.executescript("drop table if exists "+destTable)
+    cur.execute("create table "+destTable+"(genre text, date text, tempoSlowPercent float , tempoMediumPercent float , tempoFastPercent float , energyHighPercent float , energyLowPercent float )")
+    genres = getGenres(table)
     dates = getDates()
     for date in dates:
         for genre in genres:
-            sumTempo = str(cur.execute("select sum(tempo) from songs join songRate on songs.id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"'").fetchall()[0][0])
+            sumTempo = str(cur.execute("select sum(tempo) from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"'").fetchall()[0][0])
             if sumTempo == "None":
                 sumTempo = str(1)
-            sumEnergy = str(cur.execute("select sum(energy) from songs join songRate on songs.id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"'").fetchall()[0][0])
+            sumEnergy = str(cur.execute("select sum(energy) from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"'").fetchall()[0][0])
             if sumEnergy == "None":
                 sumEnergy = str(1)
-            cur.execute("insert into dailyGenrePercents (genre, date, tempoSlowPercent, tempoMediumPercent, tempoFastPercent, energyHighPercent, energyLowPercent) values (?, ?,"+
-                        "(select sum(tempo)/"+sumTempo+"*100 from songs join songRate on songs.id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and tempoDisc like 'slow')," +
-                        "(select sum(tempo)/"+sumTempo+"*100 from songs join songRate on songs.id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and tempoDisc like 'medium')," +
-                        "(select sum(tempo)/"+sumTempo+"*100 from songs join songRate on songs.id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and tempoDisc like 'fast')," +
-                        "(select sum(energy)/"+sumEnergy+"*100 from songs join songRate on songs.id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and energyDisc like 'high')," +
-                        "(select sum(energy)/"+sumEnergy+"*100 from songs join songRate on songs.id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and energyDisc like 'low'))", [genre, str(date)])
+            cur.execute("insert into "+destTable+" (genre, date, tempoSlowPercent, tempoMediumPercent, tempoFastPercent, energyHighPercent, energyLowPercent) values (?, ?,"+
+                        "(select sum(tempo)/"+sumTempo+"*100 from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and tempoDisc like 'slow')," +
+                        "(select sum(tempo)/"+sumTempo+"*100 from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and tempoDisc like 'medium')," +
+                        "(select sum(tempo)/"+sumTempo+"*100 from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and tempoDisc like 'fast')," +
+                        "(select sum(energy)/"+sumEnergy+"*100 from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and energyDisc like 'high')," +
+                        "(select sum(energy)/"+sumEnergy+"*100 from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and energyDisc like 'low'))", [genre, str(date)])
 
 
 #start main code
+
 data = spotifyChartsAPI.get_charts('2018-10-01', '2019-10-31', region='il')
-print(list(data))
 
 with sqlite3.connect("spotifyGraphDB.sqlite") as con:
     cur = con.cursor()
@@ -92,10 +91,5 @@ with sqlite3.connect("spotifyGraphDB.sqlite") as con:
     cur.executescript("drop table if exists songRate")
     cur.execute("create table songRate(id text,streams int, rateDate text,position int,region text)")
     data.apply(lambda row : rowInsert(cur, row), axis=1)
-    calculatePercents(cur)
-
-# data["Information"] = data.apply(lambda row: sp.track(row['URL']), axis=1)
-# print(data)
-# print(sp.audio_features(tracks=['1rgnBhdG2JDFTbYkYRZAku']))
-# print(sp.track('1rgnBhdG2JDFTbYkYRZAku'))
-# print(sp.artist('2NjfBq1NflQcKSeiDooVjY'))
+    calculatePercents(cur, "songs", "dailyGenrePercents")
+    calculatePercents(cur,"normalizedSongs","normalizedDailyGenrePercents")
