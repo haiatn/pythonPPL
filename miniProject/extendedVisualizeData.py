e42ee9bfe5d788c9f7ba0d4225ca017cf4f9b032
nimrodhaiat <nimnimch@gmail.com>
2019-12-22 13:15:43 +0200

    started comments

--- miniProject/extendedVisualizeData.py
+++ miniProject/extendedVisualizeData.py
@@ -6,10 +6,19 @@ from bokeh.layouts import column
 from bokeh.io import curdoc
 import colorcet as cc
 '''
+this class is the main execution class. its purpose is to collect the wanted data from the 
+database and to insert it to a the dynamic graphs that are seen when running this class as 
+a bokeh server.
+'''
 
 
 '''
-
+input:
+:param type-can be either "full" or "normalized". full means that we get all of the 
+genres and normalized are the genres we created that merge small genres into main genres.
+output:
+a list of genre names (as written in the database)
+'''
 def getGenres(type):
     with sqlite3.connect("spotifyGraphDB.sqlite") as con:
         con.row_factory = lambda cursor, row: row[0]
@@ -21,10 +30,26 @@ def getGenres(type):
         return cur.fetchall()
 
 
+'''
+output:
+a list that contains all of the dates that are in the databse. Because we limited the dates
+prior to the program that dates are constants.
+'''
 def getDates():
     return pd.date_range(start="2018-10-1", end="2019-10-31", freq='D').tolist()
 
-
+'''
+input:
+:param type-can be either "full" or "normalized". if the type is full then we need to fetch data from the table 'songs'
+and not the table 'normalizedSongs' which has the normalized data.
+:param genre- the genre in the database where we want the data from (should be normalized genre or not depends on the type)
+output:
+a list of numbers. each number is a popularity rank made out of the measurement we created for one day. It ordered so 
+the first number is for the 1/10/18 and the last number is the genre popularity for 31/10/19.
+The rank is a meseaurment for popularity. Because we look the the 30 most popular songs everyday so we have rankings 
+from 1 to 30. because rank 1 is more valuable than rank 30 so we needed to invert the values and also to 
+make it between [0,1]. The final meseaurement is average(ranksOfGenreSongs) where rankOfGenreSongs = (30-song.rate/30| song in genre) 
+'''
 def getPopularityByGenre(genre, type):
     with sqlite3.connect("spotifyGraphDB.sqlite") as con:
         table = 'songs'
@@ -36,7 +61,17 @@ def getPopularityByGenre(genre, type):
             "select COALESCE(avg, 0) as avg from (select distinct songRate.rateDate from songRate) as dates left join (select (30- Avg(position))/31 as avg, rateDate from (select id from " + table + " where genre like '" + genre + "') as ids join songRate on ids.id = songRate.id group by rateDate order by rateDate) as actualResult on dates.rateDate = actualResult.rateDate")
         return cur.fetchall()
 
-
+'''
+input:
+:param type- can be either "full" or "normalized". if the type is full then we need to fetch data from the table 'DailyGenrePercents'
+and not the table 'normalizedDailyGenrePercents' that has different calculations
+:param genre- the genre in the database where we want the data from (should be normalized genre or not depends on the type)
+:param title- that is the title of each number. if we look for 'energyLowPercent' that would be the title
+output:
+a list of numbers. It ordered so 
+the first number is for the 1/10/18 and the last number is the genre popularity for 31/10/19.
+The number is the percents of this title in this genre for everyday in the time interval.
+'''
 def getTooltipDataForGenre(genre, title, type):
     with sqlite3.connect("spotifyGraphDB.sqlite") as con:
         con.row_factory = lambda cursor, row: row[0]
@@ -53,8 +88,17 @@ def getTooltipDataForGenre(genre, title, type):
                 return x
 
         return list(map(lambda x: fixZeros(x), list(cur.fetchall())))
-
-
+'''
+input:
+:param type-can be either "full" or "normalized". if the type is full then we need to fetch data from the table 'songs'
+and not the table 'normalizedSongs' which has the normalized data.
+:param genre- the genre in the database where we want the data from (should be normalized genre or not depends on the type)
+output:
+a list of tuples. each tuple contains the genre inserted and its popularity for one day. It ordered so 
+the first tuple is for the 1/10/18 and the last tuple is the genre popularity for 31/10/19.
+note that this is the format needed for the graph. Note that we zeroed out the first and last days and that is because
+it makes the graphs more readable and intuitive for graphical reasons.
+'''
 def ridge(category, data, scale=1):
     data = list(zip([category] * len(data), scale * data))
 
@@ -68,24 +112,39 @@ def ridge(category, data, scale=1):
     data.insert(0, (category, 0))
     return data
 
-
+'''
+this function creates the graph. Because it is also an event function it should follow the onChange function interface
+and have 3 attributes. The first time it is called manually and other times it is called from the combo box in the GUI.
+input:
+:param attrName- when clicked the attribute that called the function is inserted here
+:param old- when the combo box changes the value before the search goes here
+:param new- when the combo box changes the value chosen after the search goes here
+output:
+it has no function output but it does change the GUI from normalized to full genres or the other way around.
+'''
 def update_plot(attrName, old, new):
     if old == new:
         return
     type = new
-
+    #getting axis values
     genres = list(getGenres(type))
     palette = [cc.rainbow[i * 15 % cc.rainbow.__len__()] for i in range(genres.__len__())]
     x = getDates()
+
+    #cleaning the website if already has stuff on it
     if curdoc().get_model_by_name('mainPlot') != None:
         curdoc().clear()
 
+    #creating main graph
     p = figure(name="mainPlot", y_range=genres, x_axis_type="datetime", x_range=(x[0], x[len(x) - 1]), plot_height=500,
                plot_width=1000, x_axis_label="date", y_axis_label="genres")
+
+    #creating tooltip structure
     p.add_tools(HoverTool(tooltips=[("low energy", "@energyLowPercent"), ("high energy", "@energyHighPercent"),
                                     ("slow tempo", "@tempoSlowPercent"), ("medium tempo", "@tempoMediumPercent"),
                                     ("fast tempo", "@tempoFastPercent")]))
 
+    #iterating through genres and adding subgraphs
     for i, genre in enumerate(reversed(genres)):
         y = ridge(genre, getPopularityByGenre(genre, type))
         source = ColumnDataSource(
@@ -94,31 +153,36 @@ def update_plot(attrName, old, new):
                       'tempoSlowPercent': getTooltipDataForGenre(genre, 'tempoSlowPercent', type),
                       'tempoMediumPercent': getTooltipDataForGenre(genre, 'tempoMediumPercent', type),
                       'tempoFastPercent': getTooltipDataForGenre(genre, 'tempoFastPercent', type)})
+
+
+        #load subgraphs
         p.patch(x='x', y='y', color=palette[i], alpha=0.6, line_color="black", source=source)
+
+        #load the tooltips
         p.scatter(x='x', y='y', color=palette[i], alpha=0.6, line_color="black", source=source, size=2)
 
+
+    #graphic configurations
     p.outline_line_color = None
     p.background_fill_color = "#efefef"
-
     p.ygrid.grid_line_color = None
     p.xgrid.grid_line_color = "#dddddd"
     p.xgrid.ticker = p.xaxis[0].ticker
-
     p.axis.minor_tick_line_color = None
     p.axis.major_tick_line_color = None
     p.axis.axis_line_color = None
-
     p.y_range.range_padding = 0.2
 
+    #date range bar at the bottom is initanalized here
     select = figure(title="Drag the middle and edges of the selection box to change the range above",
                     plot_height=130, plot_width=800, y_range=p.y_range,
                     x_axis_type="datetime", y_axis_type=None,
                     tools="", toolbar_location=None, background_fill_color="#efefef", name="rangePlot")
 
+    #date range bar configurations
     range_tool = RangeTool(x_range=p.x_range)
     range_tool.overlay.fill_color = "navy"
     range_tool.overlay.fill_alpha = 0.2
-
     select.line('x', 'y', source=source)
     select.ygrid.grid_line_color = None
     select.add_tools(range_tool)
@@ -132,4 +196,8 @@ def update_plot(attrName, old, new):
 
 
 ###########################################Main
+'''
+We first run this manually and it will automatically build on our server the graphs and from now on 
+all the changes will be using the combo box
+'''
 update_plot(None, None, 'Normalized')
--- miniProject/prepareData.py
+++ miniProject/prepareData.py
@@ -5,12 +5,24 @@ import sqlite3
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 
+'''
+this class works with API's and is used to build the database. It takes about 4 hours.
+we create a database of songs and artists from the 1-10-18 to 31-10-19 listening patterns of 
+Israeli citizens. 
+'''
 
+# authentication with spotify. Note that this is confidential
 os.environ["SPOTIPY_CLIENT_ID"] = "1b73550cbd9144149ec64aff3a6fc3fc"
 os.environ["SPOTIPY_CLIENT_SECRET"] = "aa0fa9dbf7de4ab7b653f15da895c3e3"
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+'''
+input:
+:param table- the table to take genres from. Because we create two table we must specify if 'songs' or 'normalizedSongs'
+output:
+list of the string names of genres that in the table given
+'''
 def getGenres(table):
     with sqlite3.connect("spotifyGraphDB.sqlite") as con:
         con.row_factory = lambda cursor, row: row[0]
@@ -18,15 +30,35 @@ def getGenres(table):
         cur.execute("select distinct genre from "+table)
         return cur.fetchall()
 
+'''
+output:
+a list that contains all of the dates that are in the databse. Because we limited the dates
+prior to the program that dates are constants.
+'''
 def getDates():
     return pd.date_range(start="2018-10-1", end="2019-10-31", freq='D').tolist()
 
+'''
+this function creates discretization of energy values. each value can be determined to be 'high' or 'low'
+input:
+:param energy- values given on scale [0,1]
+ouput:
+the discrite value chosen for the input. if the number is higher than 0.6 then it is high, else it is low.
+'''
 def getEneryDisc(energy):
     if energy > 0.6:
         return "high"
     else:
         return "low"
 
+'''
+this function creates discretization of tempo values. each value can be determined to be 'fast', 'medium or 'slow'
+input:
+:param tempo- values which are bpm (beats per minute). mostly in range (40-150)
+ouput:
+the discrite value chosen for the input. the discretization conditions are decided after web research about
+music bpm count.
+'''
 def getTempoDisc(tempo):
     if tempo > 120:
         return "fast"
@@ -35,8 +67,19 @@ def getTempoDisc(tempo):
     else:
         return "slow"
 
+'''
+this function is for generating new rows at table with new songs/artists
+input:
+:param cur- the connection to the database which we use. It should be connected from before
+:param row- the row is the object given from the API which will be analyzed
+ouput:
+the function does not return anything but it does insert to the 'songRate' table new values for one of the dates
+and if the song is not yet inserted it also added to 'songs' table with all new information.
+'''
 
 def rowInsert(cur, row):
+
+    #collecting relevant data for table
     position = row['Position']
     if position==1:
         print(str(row['date']))
@@ -45,25 +88,46 @@ def rowInsert(cur, row):
     artistName = row['Artist']
     track = sp.track(id)
     artist = track['artists'][0]
-    listOfGenres=sp.artist(artist['id'])['genres']
+    listOfGenres = sp.artist(artist['id'])['genres']
+
+    #handeling unknown genres
     if len(listOfGenres)>0:
         genre =listOfGenres[0]
     else:
         genre ="unknown"
+
+    #collecting audiofeatures (not in the regular API request)
     audioFeatures = sp.audio_features(tracks=[id])
     energy = audioFeatures[0]['energy']
     tempo = audioFeatures[0]['tempo']
     danceability=audioFeatures[0]['danceability']
+
+    #isnert to db
     cur.execute("insert into songs (id, name, artistName, genre, energy, tempo, energyDisc, tempoDisc) select * from (select '" + id + "' as id, ? as name, ? as artistName, '" + genre + "' as genre, " + str(energy) + " as energy, " + str(tempo) + " as tempo, '"+ str(getEneryDisc(energy))+"' as energyDisc, '"+ str(getTempoDisc(tempo))+"' as tempoDisc) as tmp where not exists (select * from songs where id = '" + id + "')",[name,artistName])
     streams = row['Streams']
     rateDate = str(row['date'])
     cur.execute("insert into songRate (id, streams, rateDate, position, region) values (?, ?, ?, ?,?)", [id, streams, rateDate, position, "il"])
 
+'''
+after enrating all information from API we do inside calculations of statistics to show the user.
+This means how fast/slow are songs and energy flows. We notice that we will go through discretization phase before calculations.
+input:
+:param cur- the connection to the database which we use. It should be connected from before
+:param table- the table we do statistics on (song table so it can be 'songs' or 'normalizedSongs')
+:param destTable- a table which is used for keeping all the calculation results.
+ouput:
+the function does not return anything but it does insert to the the destTable the calculated informations
+'''
 def calculatePercents(cur,table,destTable):
+    #creating table
     cur.executescript("drop table if exists "+destTable)
     cur.execute("create table "+destTable+"(genre text, date text, tempoSlowPercent float , tempoMediumPercent float , tempoFastPercent float , energyHighPercent float , energyLowPercent float )")
+
+    #getting axis value that will be input for calculations
     genres = getGenres(table)
     dates = getDates()
+
+    #iterating through each date and genre
     for date in dates:
         for genre in genres:
             sumTempo = str(cur.execute("select sum(tempo) from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"'").fetchall()[0][0])
@@ -72,6 +136,8 @@ def calculatePercents(cur,table,destTable):
             sumEnergy = str(cur.execute("select sum(energy) from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"'").fetchall()[0][0])
             if sumEnergy == "None":
                 sumEnergy = str(1)
+
+            #the actual calculations. we sum all songs and make it into percents.
             cur.execute("insert into "+destTable+" (genre, date, tempoSlowPercent, tempoMediumPercent, tempoFastPercent, energyHighPercent, energyLowPercent) values (?, ?,"+
                         "(select sum(tempo)/"+sumTempo+"*100 from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and tempoDisc like 'slow')," +
                         "(select sum(tempo)/"+sumTempo+"*100 from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and tempoDisc like 'medium')," +
@@ -80,10 +146,8 @@ def calculatePercents(cur,table,destTable):
                         "(select sum(energy)/"+sumEnergy+"*100 from "+table+" join songRate on "+table+".id = songRate.id where genre like '"+genre+"' and songRate.rateDate = '"+str(date)+"' and energyDisc like 'low'))", [genre, str(date)])
 
 
-#start main code
-
+#start main code. selecting dates, creating tables and collecting and saving to db data from APIs.
 data = spotifyChartsAPI.get_charts('2018-10-01', '2019-10-31', region='il')
-
 with sqlite3.connect("spotifyGraphDB.sqlite") as con:
     cur = con.cursor()
     cur.executescript("drop table if exists songs")
